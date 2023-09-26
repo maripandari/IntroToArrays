5bcf80a190c26d9dd20ef2e5124bfafcb18a59a7
Marijoy Jatico <142898185+maripandari@users.noreply.github.com>
2023-09-26 13:28:47 -1000

    Completed Parts 1-9

--- IntroToArrays.playground/Contents.swift
+++ IntroToArrays.playground/Contents.swift
@@ -1,3 +1,27 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+//Part 3
+
+var fruitNames: [String]
+
+//Part 4
+
+fruitNames = ["Kiwi", "Apples", "Watermelon" ]
+
+//Part 5 on your Own
+
+//Part 6
+print("I like to eat\(fruitNames)")
+
+//Part 7
+
+for name in fruitNames
+{
+    print("I like to eat " + name)
+}
+
+//Part 8
+//Part 9 On Your Own questions
+
